4ee52515cc681b57cd0c5af000cfa21ec48cae7e
carth501 <carth501@gmail.com>
2026-05-05 14:45:20 -0400

    Enhance UIManager to include throttle label management and update synchronization

--- scripts/ui_manager.gd
+++ scripts/ui_manager.gd
@@ -3,14 +3,17 @@ extends Control
 
 @export var control_interface_path: NodePath = ^"../Miracle/Node"
 @export var throttle_slider_path: NodePath = ^"VSlider"
+@export var throttle_label_path: NodePath = ^"Control/Label"
 
 var control_interface: ControlInterface
 var throttle_slider: VSlider
+var throttle_label: Label
 
 
 func _ready() -> void:
 	control_interface = get_node_or_null(control_interface_path) as ControlInterface
 	throttle_slider = get_node_or_null(throttle_slider_path) as VSlider
+	throttle_label = get_node_or_null(throttle_label_path) as Label
 
 	if control_interface == null:
 		push_warning("UIManager could not find a ControlInterface at %s" % control_interface_path)
@@ -18,12 +21,15 @@ func _ready() -> void:
 
 	if throttle_slider == null:
 		push_warning("UIManager could not find a VSlider at %s" % throttle_slider_path)
-		return
 
-	_configure_throttle_slider()
-	throttle_slider.value_changed.connect(_on_throttle_slider_changed)
+	if throttle_label == null:
+		push_warning("UIManager could not find a Label at %s" % throttle_label_path)
+
+	if throttle_slider != null:
+		_configure_throttle_slider()
+		throttle_slider.value_changed.connect(_on_throttle_slider_changed)
 	control_interface.main_throttle_changed.connect(_on_main_throttle_changed)
-	_sync_slider_to_throttle()
+	_sync_ui_to_throttle()
 
 
 func _configure_throttle_slider() -> void:
@@ -32,8 +38,11 @@ func _configure_throttle_slider() -> void:
 	throttle_slider.step = 0.01
 
 
-func _sync_slider_to_throttle() -> void:
-	throttle_slider.value = control_interface.get_main_throttle()
+func _sync_ui_to_throttle() -> void:
+	var throttle_value := control_interface.get_main_throttle()
+	if throttle_slider != null:
+		throttle_slider.value = throttle_value
+	_update_throttle_label(throttle_value)
 
 
 func _on_throttle_slider_changed(value: float) -> void:
@@ -41,4 +50,13 @@ func _on_throttle_slider_changed(value: float) -> void:
 
 
 func _on_main_throttle_changed(value: float) -> void:
-	throttle_slider.value = value
+	if throttle_slider != null:
+		throttle_slider.value = value
+	_update_throttle_label(value)
+
+
+func _update_throttle_label(value: float) -> void:
+	if throttle_label == null:
+		return
+
+	throttle_label.text = "%d%%" % int(round(value * 100.0))
